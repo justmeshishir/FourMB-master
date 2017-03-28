ac2ce421ce54577f6ff5082ce5e937867e7f7d37
Amrit Twanabasu <amrit.cas@gmail.com>
2017-03-28 07:48:06 +0545

    Minor fixes

--- js/app.js
+++ js/app.js
@@ -31,20 +31,6 @@ function shadeBlendConvert(p, from, to) {
     else return "#"+(0x100000000+(f[3]>-1&&t[3]>-1?r(((t[3]-f[3])*p+f[3])*255):t[3]>-1?r(t[3]*255):f[3]>-1?r(f[3]*255):255)*0x1000000+r((t[0]-f[0])*p+f[0])*0x10000+r((t[1]-f[1])*p+f[1])*0x100+r((t[2]-f[2])*p+f[2])).toString(16).slice(f[3]>-1||t[3]>-1?1:3);
 }
 
-function updateSkillsStyles() {
-    var skills = document.querySelectorAll('.skill-list__item');
-    var maxFontSize = 20;
-    var minFontSize = 14;
-    var skillCount = skills.length;
-
-    for(var i = 1; i <= skillCount; i++) {
-        var element = skills[i - 1];
-        var itemFontSize = (1 - (i / skillCount)) * (maxFontSize - minFontSize) + minFontSize;
-        var itemColor = shadeBlendConvert(i / skillCount, "#263238", "#607D8B");
-        element.setAttribute("style", element.getAttribute("style") + ";font-size:" + itemFontSize + "px;color:" + itemColor + ";");             
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function(event) { 
     var targetItem, targetOffset, currentPosition,
     body = document.body,
@@ -54,15 +40,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     for(var i in links) {
         links[i].onclick = function(event) {
-            console.log(event.target.hash.substr(1));
             var targetHash = event.target.hash.substr(1);
             targetItem = document.getElementById(targetHash);
 
             if(!targetItem) {
                 targetItem = document.querySelector('a[name="' + targetHash + '"]');
             }
-
-            console.log(targetItem, targetItem.offsetTop);
 
             targetOffset = targetItem.offsetTop;
             currentPosition = getPageScroll();
